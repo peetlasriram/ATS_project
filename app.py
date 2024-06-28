092d93adb3178745619eb3020388c5dbeb2a5709
Sriram <peetlasreeamulu40@gmil.com>
2024-06-28 19:20:50 +0530

    code modifaction

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
-load_dotenv()
 
-import streamlit as st 
+load_dotenv()
+import base64
+import streamlit as st
 import os
-from PIL import Image
+import io
+from PIL import Image 
 import pdf2image
 import google.generativeai as genai
 
